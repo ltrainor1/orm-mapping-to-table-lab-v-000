80592b29ac6807284361b4841e6f0866c7da6521
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-17 16:18:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ end
 def save
   sql = "INSERT INTO students (name, grade) VALUES(?,?)"
   DB[:conn].execute(sql, self.name, self.grade)
-  self.id = DB[:conn].execute("SELECT MAX(id) FROM students")[0].to_i
+  @id = DB[:conn].execute("SELECT MAX(id) FROM students")[0].text
 end
 
 end
