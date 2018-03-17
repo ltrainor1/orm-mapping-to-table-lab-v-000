8fd556438585707c0eb8810cd1b473efd7284c88
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-17 16:05:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ def self.drop_table
 end
 
 def save
-  sql = "INSERT INTO students (name, grade) VALUES(?,?)"
+  sql = "INSERT INTO students (name, grade) OUTPUT INSERTED.ID VALUES(?,?)"
   DB[:conn].execute(sql, self.name, self.grade)
   self.id = DB[:conn].execute("RETURN id FROM students WHERE name = #{self.name}")
 end
